fef9735d8064107e7fbd2153f25f59c65ac5bd1b
Bombay <OMGmsebastian@gmail.com>
2023-10-03 18:50:47 -0400

    feat(core): setup files

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'fun-core'
+description 'A simple core for FiveM'
+license 'MIT'
+author 'BombayV'
+repository 'https://github.com/BombayV/fun-core'
+
+dependencies {
+  '/onesync'
+}
+
+client_script 'build/client.js'
+server_script 'build/server.js'
---
+++ server/index.ts
@@ -0,0 +1,1 @@
+console.log('hello world, from fun-core/server/index.ts')
